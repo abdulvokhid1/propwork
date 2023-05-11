a1b1116665e1d978efcfb6f1ac6e20763e518bd9
abdulvokhid <73060118+abdulvokhid1@users.noreply.github.com>
2023-05-12 03:18:46 +0900

    added

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,9 +1,46 @@
 /** @format */
 
 import React from "react";
-import { Wrapper } from "./styled";
+import {
+  Bigpic,
+  Buywrapper,
+  Definition,
+  Features,
+  LocWrapper,
+  Smalldef,
+  Table,
+  Word,
+  Wrapper,
+} from "./styled";
+import building from "../../Assets/img/building.png";
 
 const Home = () => {
-  return <Wrapper>Home</Wrapper>;
+  return (
+    <Wrapper>
+      <Definition>
+        <Word>Find the perfect place to stay with your family</Word>
+        <Smalldef>
+          Buying a home is a life-changing experience, so shouldn’t your real
+          estate agent be a life changer.
+        </Smalldef>
+        <Table>
+          <Buywrapper>
+            <Features>Buy</Features>
+            <Features>Rent</Features>
+            <Features>Sell</Features>
+          </Buywrapper>
+          <LocWrapper>
+            <div>hello</div>
+            <div>hello</div>
+            <div>hello</div>
+            <div>hello</div>
+          </LocWrapper>
+        </Table>
+      </Definition>
+      <Bigpic>
+        <img src={building} />
+      </Bigpic>
+    </Wrapper>
+  );
 };
 export default Home;
