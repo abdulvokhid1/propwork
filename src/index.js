88cc1ebfb6a912bbb44ab91827eea1ae62d6625d
Abdulvokhid Ibrakhimov <ibrakhimov7747a@gmail.com>
2023-06-07 02:44:53 +0900

    added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { Root } from './Root';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Lessons from "./Lessons";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Root />
+    <Lessons />
   </React.StrictMode>
 );
 
