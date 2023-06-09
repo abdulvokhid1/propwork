752fc6da00de1229c1ebd164363823ba9ea08487
Abdulvokhid Ibrakhimov <ibrakhimov7747a@gmail.com>
2023-06-09 17:12:28 +0900

    committed

--- src/Lessons/useState/index.jsx
+++ src/Lessons/useState/index.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState({
+    name: "",
+    surname: "",
+    address: "",
+  });
   return (
     <div>
       <button
@@ -11,7 +15,6 @@ const Home = () => {
       >
         increase
       </button>
-      {count}
       <button
         onClick={() => {
           setCount(count - 1);
@@ -19,6 +22,44 @@ const Home = () => {
       >
         decrease
       </button>
+      <div>
+        <input
+          type="text"
+          placeholder="name"
+          onChange={(e) => {
+            setCount({
+              name: e.target.value,
+              surname: count.surname,
+              address: count.address,
+            });
+          }}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="surname"
+          onChange={(e) => {
+            setCount({
+              name: count.name,
+              surname: e.target.value,
+              address: count.address,
+            });
+          }}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="address"
+          onChange={(e) => {
+            setCount({
+              name: count.name,
+              surname: count.surname,
+              address: e.target.value,
+            });
+          }}
+        />
+        {JSON.stringify(count)}
+      </div>
     </div>
   );
 };
