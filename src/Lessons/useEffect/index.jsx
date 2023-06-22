6adf904d7719ad93e2218a3a56096298d67cfb8a
Abdulvokhid Ibrakhimov <ibrakhimov7747a@gmail.com>
2023-06-23 03:04:30 +0900

    added

--- src/Lessons/useEffect/index.jsx
+++ src/Lessons/useEffect/index.jsx
@@ -2,19 +2,26 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 
 const AppuseEffect = () => {
-  const [response, setResponse] = useState([]);
+  const [getresponse, setgetResponse] = useState([]);
+  const [postCount, setPostCount] = useState(10);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
+    fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${postCount}`)
       .then((response) => response.json())
-      .then((json) => console.log(json));
-  }, []);
+      .then((json) => setgetResponse(json));
+  }, [postCount]);
   return (
     <div>
       <div>
-        {response.map((value)=>{
-          return 
+        {getresponse.map((value) => {
+          return (
+            <div style={{ display: "flex" }} key={value.id}>
+              <h1>{value.id}</h1>
+              <h1>{value.title}</h1>
+            </div>
+          );
         })}
       </div>
+      <button onClick={() => setPostCount(postCount + 10)}>add 10</button>
     </div>
   );
 };
