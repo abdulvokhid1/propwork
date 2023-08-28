21c691c440bb36a7f94f17c8a9e6d8bac674e926
Abdulvokhid Ibrakhimov <ibrakhimov7747a@gmail.com>
2023-08-28 16:41:41 +0900

    added

--- src/components/Add/index.jsx
+++ src/components/Add/index.jsx
@@ -1,30 +1,42 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { MovieContext } from "../../context/movieContext";
 
 const Add = () => {
+  const [data, setData] = useContext(MovieContext);
+
   const [name, setName] = useState("");
   const [author, setAuthor] = useState("");
   const [published, setPublished] = useState("");
 
   const onSubmit = () => {
     console.log(name, author, published);
+    setData([
+      ...data,
+      {
+        id: data.length,
+        name: name,
+        author: author,
+        published: published ? "" : alert("fill all"),
+      },
+    ]);
   };
   return (
     <div>
       <input
         value={name}
-        onClick={(e) => setName(e.target.value)}
+        onChange={(e) => setName(e.target.value)}
         placeholder="name"
         name={"name"}
       />
       <input
-        // value={author}
-        onClick={(e) => setAuthor(e.target.value)}
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
         placeholder="author"
         name={"author"}
       />
       <input
-        // value={published}
-        onClick={(e) => setPublished(e.target.value)}
+        value={published}
+        onChange={(e) => setPublished(e.target.value)}
         placeholder="published"
         name={"published"}
       />
